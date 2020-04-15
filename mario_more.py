d858d4ff028ca92dd3ae1883ba6d1901f88351d7
Brant-L <61823767+Brant-L@users.noreply.github.com>
2020-04-15 07:08:33 -0600

    Add files via upload

---
+++ mario_more.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+# Prompt for integer between 1 and 8 inclusive
+x = 0
+while x < 1 or x > 8:
+    x = get_int("Enter pyramid height:\n")
+
+# Loop through each row to print correct number of spaces and hashes
+for i in range(1, x + 1, 1):
+    print(" " * (x - i) + "#" * i + "  " + "#" * i)
